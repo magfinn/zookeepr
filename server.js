c14365acb546b986cb6e1d5bd42bdaae370dc249
Maggie Finnegan <wisner.maggiel@gmail.com>
2021-12-12 16:23:04 -0800

    connected front end public code with server. Animals and homepage display properly

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//middleware that instructs the server to make certain files readily available and to not gate it behind a server endpoint.
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -97,6 +99,22 @@ app.get('/api/animals/:id', (req, res) => {
     }
   });
 
+  app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  
+  app.get('/zookeepers', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
 app.post('/api/animals', (req,res) => {
     //set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
@@ -109,10 +127,6 @@ app.post('/api/animals', (req,res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(req.body);
     }
-});
-
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, './public.index.html'));
 });
 
 app.listen(PORT,() => {
